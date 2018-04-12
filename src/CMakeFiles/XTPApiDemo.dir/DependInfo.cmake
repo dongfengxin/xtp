7287a7e9b26a8ed5d593d005cada450faf50a26b
zklvyy <zklvyy@outlook.com>
2018-04-12 12:29:23 +0800

    use cmake bulid linux success

---
+++ src/CMakeFiles/XTPApiDemo.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/root/xtp/src/FileUtils.cpp" "/root/xtp/src/CMakeFiles/XTPApiDemo.dir/FileUtils.cpp.o"
+  "/root/xtp/src/quote_spi.cpp" "/root/xtp/src/CMakeFiles/XTPApiDemo.dir/quote_spi.cpp.o"
+  "/root/xtp/src/trade_spi.cpp" "/root/xtp/src/CMakeFiles/XTPApiDemo.dir/trade_spi.cpp.o"
+  "/root/xtp/src/xtp_api_demo.cpp" "/root/xtp/src/CMakeFiles/XTPApiDemo.dir/xtp_api_demo.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "api"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
