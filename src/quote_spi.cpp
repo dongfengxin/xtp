d6f4c506ad4ef4467331d63fbcd69fd16772a4ba
zklvyy <zklvyy@outlook.com>
2018-04-13 12:22:49 +0800

    fromat all file

--- src/quote_spi.cpp
+++ src/quote_spi.cpp
@@ -3,12 +3,11 @@
 #include <stdio.h>
 using namespace std;
 
-
-
-void MyQuoteSpi::OnError(XTPRI *error_info, bool is_last)
+void MyQuoteSpi::OnError(XTPRI* error_info, bool is_last)
 {
-	cout << "--->>> "<< "OnRspError" << endl;
-	IsErrorRspInfo(error_info);
+    cout << "--->>> "
+	 << "OnRspError" << endl;
+    IsErrorRspInfo(error_info);
 }
 
 MyQuoteSpi::MyQuoteSpi()
@@ -21,96 +20,91 @@ MyQuoteSpi::~MyQuoteSpi()
 
 void MyQuoteSpi::OnDisconnected(int reason)
 {
-	cout << "--->>> " << "OnDisconnected quote" << endl;
-	cout << "--->>> Reason = " << reason << endl;
-	//断线后，可以重新连接
-	//重新连接成功后，需要重新向服务器发起订阅请求
+    cout << "--->>> "
+	 << "OnDisconnected quote" << endl;
+    cout << "--->>> Reason = " << reason << endl;
+    //断线后，可以重新连接
+    //重新连接成功后，需要重新向服务器发起订阅请求
 }
 
-void MyQuoteSpi::OnSubMarketData(XTPST *ticker, XTPRI *error_info, bool is_last)
+void MyQuoteSpi::OnSubMarketData(XTPST* ticker, XTPRI* error_info, bool is_last)
 {
- 	cout << "OnRspSubMarketData -----" << endl;
+    cout << "OnRspSubMarketData -----" << endl;
+    IsErrorRspInfo(error_info);
 }
 
-void MyQuoteSpi::OnUnSubMarketData(XTPST *ticker, XTPRI *error_info, bool is_last)
+void MyQuoteSpi::OnUnSubMarketData(XTPST* ticker, XTPRI* error_info, bool is_last)
 {
- 	cout << "OnRspUnSubMarketData -----------" << endl;
+    cout << "OnRspUnSubMarketData -----------" << endl;
 }
 
-void MyQuoteSpi::OnDepthMarketData(XTPMD * market_data, int64_t bid1_qty[], int32_t bid1_count, int32_t max_bid1_count, int64_t ask1_qty[], int32_t ask1_count, int32_t max_ask1_count)
-{
-}
-
-void MyQuoteSpi::OnSubOrderBook(XTPST *ticker, XTPRI *error_info, bool is_last)
-{
-
-}
-
-void MyQuoteSpi::OnUnSubOrderBook(XTPST *ticker, XTPRI *error_info, bool is_last)
-{
-
-}
-
-void MyQuoteSpi::OnSubTickByTick(XTPST *ticker, XTPRI *error_info, bool is_last)
-{
-
-}
-
-void MyQuoteSpi::OnUnSubTickByTick(XTPST * ticker, XTPRI * error_info, bool is_last)
+void MyQuoteSpi::OnDepthMarketData(XTPMD* market_data, int64_t bid1_qty[], int32_t bid1_count, int32_t max_bid1_count, int64_t ask1_qty[], int32_t ask1_count, int32_t max_ask1_count)
 {
 }
 
-void MyQuoteSpi::OnOrderBook(XTPOB *order_book)
-{
-
-}
-
-void MyQuoteSpi::OnTickByTick(XTPTBT *tbt_data)
-{
-
-}
-
-void MyQuoteSpi::OnQueryAllTickers(XTPQSI * ticker_info, XTPRI * error_info, bool is_last)
-{
-	cout << "OnQueryAllTickers -----------" << endl;
-}
-
-void MyQuoteSpi::OnSubscribeAllMarketData(XTPRI * error_info)
+void MyQuoteSpi::OnSubOrderBook(XTPST* ticker, XTPRI* error_info, bool is_last)
 {
 }
 
-void MyQuoteSpi::OnUnSubscribeAllMarketData(XTPRI *error_info)
-{
-
-}
-
-void MyQuoteSpi::OnSubscribeAllOrderBook(XTPRI *error_info)
-{
-
-}
-
-void MyQuoteSpi::OnUnSubscribeAllOrderBook(XTPRI * error_info)
+void MyQuoteSpi::OnUnSubOrderBook(XTPST* ticker, XTPRI* error_info, bool is_last)
 {
 }
 
-void MyQuoteSpi::OnSubscribeAllTickByTick(XTPRI * error_info)
+void MyQuoteSpi::OnSubTickByTick(XTPST* ticker, XTPRI* error_info, bool is_last)
 {
 }
 
-void MyQuoteSpi::OnUnSubscribeAllTickByTick(XTPRI * error_info)
+void MyQuoteSpi::OnUnSubTickByTick(XTPST* ticker, XTPRI* error_info, bool is_last)
 {
 }
 
-void MyQuoteSpi::OnQueryTickersPriceInfo(XTPTPI * ticker_info, XTPRI * error_info, bool is_last)
+void MyQuoteSpi::OnOrderBook(XTPOB* order_book)
 {
 }
 
-bool MyQuoteSpi::IsErrorRspInfo(XTPRI *pRspInfo)
+void MyQuoteSpi::OnTickByTick(XTPTBT* tbt_data)
 {
-	// 如果ErrorID != 0, 说明收到了错误的响应
-	bool bResult = ((pRspInfo) && (pRspInfo->error_id != 0));
-	if (bResult)
-		cout << "--->>> ErrorID=" << pRspInfo->error_id << ", ErrorMsg=" << pRspInfo->error_msg << endl;
-	return bResult;
+}
+
+void MyQuoteSpi::OnQueryAllTickers(XTPQSI* ticker_info, XTPRI* error_info, bool is_last)
+{
+    cout << "OnQueryAllTickers -----------" << endl;
+}
+
+void MyQuoteSpi::OnSubscribeAllMarketData(XTPRI* error_info)
+{
+}
+
+void MyQuoteSpi::OnUnSubscribeAllMarketData(XTPRI* error_info)
+{
+}
+
+void MyQuoteSpi::OnSubscribeAllOrderBook(XTPRI* error_info)
+{
+}
+
+void MyQuoteSpi::OnUnSubscribeAllOrderBook(XTPRI* error_info)
+{
+}
+
+void MyQuoteSpi::OnSubscribeAllTickByTick(XTPRI* error_info)
+{
+}
+
+void MyQuoteSpi::OnUnSubscribeAllTickByTick(XTPRI* error_info)
+{
+}
+
+void MyQuoteSpi::OnQueryTickersPriceInfo(XTPTPI* ticker_info, XTPRI* error_info, bool is_last)
+{
+}
+
+bool MyQuoteSpi::IsErrorRspInfo(XTPRI* pRspInfo)
+{
+    // 如果ErrorID != 0, 说明收到了错误的响应
+    bool bResult = ((pRspInfo) && (pRspInfo->error_id != 0));
+    if (bResult)
+	cout << "--->>> ErrorID=" << pRspInfo->error_id << ", ErrorMsg=" << pRspInfo->error_msg << endl;
+    return bResult;
 }
 
